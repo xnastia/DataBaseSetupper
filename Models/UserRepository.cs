5ac867d19687f48f128d0b4084ad91e4899abff0
Anastasiia Prygun <anastasiia.prygun@gmail.com>
2018-05-18 14:55:51 +0300

    Added UserRepository

--- Models/UserRepository.cs
+++ Models/UserRepository.cs
@@ -21,5 +21,43 @@ namespace Models
             }
             return users;
         }
+        public User Get(int id)
+        {
+            User user = null;
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                user = db.Query<User>("SELECT * FROM Users WHERE Id = @id", new { id }).FirstOrDefault();
+            }
+            return user;
+        }
+ 
+        public User Create(User user)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = "INSERT INTO Users (Name, Age) VALUES(@Name, @Age); SELECT CAST(SCOPE_IDENTITY() as int)";
+                int userId = db.Query<int>(sqlQuery, user).FirstOrDefault();
+                user.Id = userId;
+            }
+            return user;
+        }
+ 
+        public void Update(User user)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+            {
+                var sqlQuery = "UPDATE Users SET Name = @Name, Age = @Age WHERE Id = @Id";
+                db.Execute(sqlQuery, user);
+            }
+        }
+ 
+        public void Delete(int id)
+        {
+             using (IDbConnection db = new SqlConnection(connectionString))
+             {
+                 var sqlQuery = "DELETE FROM Users WHERE Id = @id";
+                 db.Execute(sqlQuery, new { id });
+             }
+        }
     }
 }
